290e9856eb253a6e2e9bbb7f20d466bd7b8265f6
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-05 18:12:30 +0300

    C# calismalar

---
+++ AdoNetDemo/AdoNetDemo/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        ProductDal _productDal = new ProductDal();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productDal.Add(new Product
+                {
+                    Name = tbxName.Text,
+                    UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                    StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+                });
+                MessageBox.Show("Product Added");
+                
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please Fill All Inputs Correctly");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"An Error Occoured: {exception.GetType()}");
+            }
+            finally
+            {
+                LoadProducts();
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productDal.Update(new Product
+                {
+                    Id = Convert.ToInt32(tbxIdUpdate.Text),
+                    Name = tbxNameUpdate.Text.ToString(),
+                    UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text),
+                    StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+                });
+                MessageBox.Show("Product Updated");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please Fill All Inputs Correctly");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"An Error Occoured: {exception.GetType()}");
+            }
+            finally
+            {
+                LoadProducts();
+            }
+
+        }
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxIdUpdate.Text = dgwProducts.CurrentRow.Cells[0].Value.ToString();
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value));
+            MessageBox.Show("The entry has removed.");
+            LoadProducts();
+        }
+    }
+}
---
+++ AdoNetDemo/AdoNetDemo/ProductDal.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        static SqlConnection _connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+        public List<Product> GetAll()
+        {
+            checkConnection();
+            SqlCommand command = new SqlCommand("SElect * from Products", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                products.Add(new Product
+                {
+                    Id = Convert.ToInt32(reader["ID"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                });
+            }
+
+            reader.Close();
+            closeConnection();
+            return products;
+        }
+        public void Add(Product product)
+        {
+            RunQuery(() => {
+                AddProduct(product);
+            });
+        }
+        public void Update(Product product)
+        {
+            RunQuery(() =>{
+                UpdateProduct(product);
+            });
+        }
+        public void Delete(int id)
+        {
+            RunQuery(() => {
+                DeleteFrom(id);
+            });
+        }
+        private void RunQuery(Action action)
+        {
+            checkConnection();
+            action.Invoke();
+            closeConnection();
+        }
+        private void UpdateProduct(Product product)
+        {
+            SqlCommand command = new SqlCommand("Update Products Set Name=@name, UnitPrice=@unitPrice, StockAmount=@stockAmount WHERE Id=@id", _connection);
+            command.Parameters.AddWithValue("@Id", product.Id);
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            command.ExecuteNonQuery();
+        }
+        private void AddProduct(Product product)
+        {
+            SqlCommand command = new SqlCommand("Insert into Products values(@name,@unitPrice,@stockAmount)", _connection);
+            command.Parameters.AddWithValue("@name", product.Name);
+            command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            command.ExecuteNonQuery();
+        }
+        private void DeleteFrom(int id)
+        {
+            SqlCommand command = new SqlCommand("Delete From Products Where Id=@id", _connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+        }
+        private void checkConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+        private void closeConnection()
+        {
+            _connection.Close();
+        }
+    }
+}
