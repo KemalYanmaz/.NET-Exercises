0e0020abe005241d042651464af9f728183008e6
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-06 00:43:40 +0300

    Generics

---
+++ GenericExercises/GenericExercises/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericExercises
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> cities= utilities.BuildList<string>("Ankara", "İzmir", "Adana");
+            List<int> numbers = utilities.BuildList<int>(1, 2, 3, 4);
+
+        }
+
+        class Utilities
+        {
+            public List<T> BuildList<T>(params T[] items){
+                return new List<T>(items);
+            }
+        }
+    }
+}
