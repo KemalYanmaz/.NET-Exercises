2093de3be2db15f59238139fb2b7dcf86693564c
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-10 23:57:45 +0300

    Attributes

---
+++ GenericExercises/Attributes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Attiributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id=1};
+            new CustomerDal().Add(customer);
+        }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+       // [RequiredProperty]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.Id},{customer.FirstName},{customer.LastName},{customer.Age} added");
+        }
+    }
+}
