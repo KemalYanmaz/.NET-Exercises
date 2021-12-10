267b8c4eb8028e85b55027c30bcfae4abf0ea410
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-10 23:58:32 +0300

    .

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,11 +4,11 @@ using System;
 namespace ConsoleApp1
 {
     class Program
-{
-    static void Main(String[] args)
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
+        }
     }
-}
 }
