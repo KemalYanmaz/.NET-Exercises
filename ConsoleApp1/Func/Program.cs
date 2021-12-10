243431c67e0c37f53d9ed00ac08f7c97e39ae441
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-10 23:57:04 +0300

    Funcs

---
+++ ConsoleApp1/Func/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = Topla;
+            add += Cikar;
+            Console.WriteLine(add(3, 5));
+
+            Func<int> getRandomNumber = delegate
+            {
+                return new Random().Next(1, 100);
+            };
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+        }
+ 
+        static int Topla(int x,int y)
+        {
+            return x + y;
+        }
+        
+        static int Cikar(int x,int y)
+        {
+            return x - y;
+        }
+    }
+}
