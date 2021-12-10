9c8719e49a5c4e7f11cd40f40fc29267d7aca962
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-11 02:18:12 +0300

    Completed

--- NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
+++ NLayeredAppDemo/Northwind.WebFormsUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Northwind.Business.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,16 @@ namespace Northwind.WebFormsUI
 {
     public partial class Form1 : Form
     {
+        private ProductManager _productManager;
         public Form1()
         {
+            _productManager = new ProductManager();
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgwProduct.DataSource = _productManager.GetAll();
         }
     }
 }
