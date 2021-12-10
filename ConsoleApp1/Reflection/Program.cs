136bef0c8b845a26e4f123cd12245cdbb5e58e0a
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-10 23:57:23 +0300

    Reflection

---
+++ ConsoleApp1/Reflection/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(DortIslem);
+            var instance = Activator.CreateInstance(type,8,7);
+            foreach (var item in instance.GetType().GetMethods())
+            {
+                string x = String.Empty;
+                x += item.Name+"(";
+                foreach (var item2 in item.GetParameters())
+                {
+                    x += ", ";
+                    x += item2;
+                }
+                x+=")";
+                Console.WriteLine(x);
+            }
+        }
+    }
+    
+    public class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+
+        public DortIslem()
+        {
+
+        }
+        public DortIslem(int sayi1,int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Topla()
+        {
+            return _sayi1 + _sayi2;
+        }
+
+        public int Carp()
+        {
+            return _sayi1 * _sayi2;
+        }
+
+    }
+}
