fb0669d99acc744a206263d102a36e7ba66620fa
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-11 18:20:44 +0300

    Design Patterns

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = CustomerManager.GetInstance();
+            customerManager.Save("Kemal");
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _INSTANCE;
+        static object _lockObject = new object();
+        private CustomerManager()
+        {}
+
+        public static CustomerManager GetInstance()
+        {
+            lock (_lockObject)
+            {
+                if (_INSTANCE == null)
+                {
+                    _INSTANCE = new CustomerManager();
+                }
+            }
+            return _INSTANCE;
+        }
+        
+        public void Save(string customer)
+        {
+            Console.WriteLine($"Customer {customer} Saved");
+        }
+    }
+}
