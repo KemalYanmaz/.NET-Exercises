d8bb6517ff5e4d6ef7dfa6220c67c3ccfda91001
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-12 01:55:16 +0300

    Composite DesignPattern Added

---
+++ DesignPatterns/Composite/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace Composite
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee kemal = new Employee { Name = "Kemal Yanmaz" };
+            Employee ulas= new Employee { Name = "Ulaş Yanmaz" };
+            Employee engin = new Employee { Name = "Engin Demiroğ" };
+            kemal.AddSubordinate(ulas);
+            kemal.AddSubordinate(engin);
+            foreach (Employee manager in kemal)
+            {
+                Console.WriteLine(manager.Name);
+                foreach (var employee in manager)
+                {
+                    Console.Write(employee.Name);
+                }
+            }
+        }
+    }
+
+    public interface IPerson
+    {
+        string Name { get; set; }
+    }
+
+    public class Employee : IPerson, IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new List<IPerson>();
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+
+        public IPerson GetSubordinate(int index)
+        {
+            return _subordinates[index];
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
