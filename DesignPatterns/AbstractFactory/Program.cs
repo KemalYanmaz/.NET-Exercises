79f5c1ff532c63082cd9f68b02692a9c098d54d7
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-12 00:11:34 +0300

    AbstractFactory Design Pattern Added

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace AbstractFactory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory2());
+            productManager.GetAll();
+        }
+    }
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.Write("Logged with Log4Net");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.Write("Logged with NLogger");
+        }
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with MemCache");
+        }
+    }
+    public class RedisCache: Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with RedisCache");
+        }
+    }
+
+    public abstract class CrossCuttingConcernsFactory1
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCuttingConcernsFactory1
+    {
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+    public class Factory2 : CrossCuttingConcernsFactory1
+    {
+        public override Caching CreateCaching()
+        {
+            return new MemCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+
+    public class ProductManager
+    {
+        private CrossCuttingConcernsFactory1 _crossCuttingConcernsFactory1;
+        private Logging _logging;
+        private Caching _caching;
+        public ProductManager(CrossCuttingConcernsFactory1 crossCuttingConcernsFactory1)
+        {
+            _crossCuttingConcernsFactory1 = crossCuttingConcernsFactory1;
+            _logging = _crossCuttingConcernsFactory1.CreateLogger();
+            _caching = _crossCuttingConcernsFactory1.CreateCaching();
+        }
+
+        public void GetAll()
+        {
+            string info = "Products listed";
+            _logging.Log(info);
+            _caching.Cache(info+"\tData");
+            Console.WriteLine(info);
+        }
+    }
+}
