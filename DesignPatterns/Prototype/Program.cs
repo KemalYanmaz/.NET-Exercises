0870ccccfb5346ceb180428707ede95cdc1acad4
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-12 00:53:25 +0300

    Prototype DesignPatern Added

---
+++ DesignPatterns/Prototype/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                City = "Ankara",
+                Id = 1
+            };
+
+            Console.WriteLine(customer1.FirstName);
+
+            Customer customer2 = (Customer)customer1.Clone();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+    public class Employee : Person
+    {
+        public decimal Salaru{ get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
