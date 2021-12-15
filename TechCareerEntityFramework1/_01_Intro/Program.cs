658c8f15c32af8e53cf40a67e0f52fb3b1146c09
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2021-12-15 20:48:47 +0300

    Intro1 Added

---
+++ TechCareerEntityFramework1/_01_Intro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=NORTHWIND;Trusted_Security=True");
+            SqlCommand command = new SqlCommand("Select * From Categories",connection);
+            connection.Open();
+            SqlDataReader  reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(1));
+            }
+
+            connection.Close();
+            
+        }
+    }
+}
