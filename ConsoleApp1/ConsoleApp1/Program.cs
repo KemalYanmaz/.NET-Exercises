cd06c021def127653c656e1463ba6c539012e8bb
Kemal Yanmaz <ulaskemalyanmaz@gmail.com>
2022-09-07 00:41:37 +0300

    merge

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            Console.WriteLine("Hello, World!");
-
-        }
-    }
-}
+﻿
